f16e9bf1f55f33e3737e8eca1d6700b8a9e4c9e0
Leggera <lana.krlv1995@gmail.com>
2018-12-17 14:48:16 +0300

    Form attempt

--- demo/views.py
+++ demo/views.py
@@ -11,3 +11,10 @@ def whoami(request):
 	name = request.GET['name']
 	response = 'You are ' + name + ' and of sex ' + sex
 	return HttpResponse(response)
+	
+def text_form(request):
+    return render(request, 'demo/text_form.html', {})
+    
+def post_new(request):
+    form = PostForm()
+    return render(request, 'demo/text_form.html', {'form': form})
